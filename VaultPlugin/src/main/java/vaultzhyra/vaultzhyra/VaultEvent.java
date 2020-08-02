95bf2c876428ba57da7aa76d53fd1ca7be1e8640
WorldwideErrors <45656346+WorldwideErrors@users.noreply.github.com>
2020-08-02 14:41:01 +0200

    Added Sounds to event

--- VaultPlugin/src/main/java/vaultzhyra/vaultzhyra/VaultEvent.java
+++ VaultPlugin/src/main/java/vaultzhyra/vaultzhyra/VaultEvent.java
@@ -1,10 +1,10 @@
 package vaultzhyra.vaultzhyra;
 
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.EnderChest;
+import org.bukkit.entity.Phantom;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,6 +22,9 @@ public class VaultEvent implements Listener {
     @EventHandler
     public void onGUIClose(InventoryCloseEvent e){
         if (e.getView().getTitle().contains(Utils.chat("&5" + e.getPlayer().getName() + "'s &0kluis"))){
+            Player p = (Player)e.getPlayer();
+            Location loc = e.getPlayer().getLocation();
+            p.playSound(loc, Sound.BLOCK_ENDER_CHEST_CLOSE,100,1);
             //Opslaan van de spullen in de kluis.
             PersonalVault.put(e.getPlayer().getUniqueId().toString(), e.getInventory().getContents());
         }
@@ -31,11 +34,13 @@ public class VaultEvent implements Listener {
     public void onECOpen(PlayerInteractEvent event){
         Player p = event.getPlayer();
         Block block = event.getClickedBlock();
+        Location loc = event.getPlayer().getLocation();
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK){
             if (block.getType().equals(Material.ENDER_CHEST)){
                 event.setCancelled(true); //Het openen van de enderchest wordt gecancelled, zodat de standaard enderchest niet geopend wordt.
                 Inventory inv = Bukkit.createInventory(p, 36, (Utils.chat("&5" + p.getName() + "'s &0kluis"))); //Custom inventory wordt gemaakt
                 p.sendMessage(Utils.chat("&5[Vault] &fKluis aan het openen...")); //Speler wordt op de hoogte gebracht van dat zijn kluis wordt geopend
+                p.playSound(loc, Sound.BLOCK_ENDER_CHEST_OPEN,100,1);
                 if (PersonalVault.containsKey(p.getUniqueId().toString())){
                     //Als de kluis niet leeg is, worden de spullen erin gelegd vanuit de map
                     inv.setContents(PersonalVault.get(p.getUniqueId().toString()));
